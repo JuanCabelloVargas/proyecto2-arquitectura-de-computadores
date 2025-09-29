3fbfae4459b7f529d90950769df36ae749ade70f
JuanCabelloVargas <jicabello@miuandes.cl>
2025-09-29 14:32:26 -0300

    feat(instr): Define format of 16-bit instruction

--- instruction_memory.v
+++ instruction_memory.v
@@ -1,11 +1,14 @@
-module instruction_memory(address, out);
-   input [3:0] address;
-   output [8:0] out;
+module instruction_memory (
+    address,
+    out
+);
+  input [7:0] address;
+  output [15:0] out;
 
-   wire [3:0]   address;
-   wire [8:0]   out;
+  wire [7:0]   address;
+  wire [15:0]   out;
 
-   reg [8:0]    mem [0:15];
+  reg [15:0]    mem [0:255];
 
-   assign out = mem[address];
+  assign out = mem[address];
 endmodule
