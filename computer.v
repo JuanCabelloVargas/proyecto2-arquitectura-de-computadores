5703604d874c17a8e78edde6f072b9f77696cf00
Matias Atria <matiasatria@gmail.com>
2025-09-30 00:56:08 -0300

    Operaciones faltantes agregadas

--- computer.v
+++ computer.v
@@ -26,178 +26,307 @@ module computer (
   reg [3:0] alu_op; // Operación de la ALU
 
   always @(*) begin
-    // Valores por defecto (NOP)
+
     LA     = 1'b0;
     LB     = 1'b0;
-    selA   = 2'b10; // 0
-    selB   = 2'b00; // B (irrelevante en NOP)
-    alu_op = 4'b0000; // ADD (sin cargas no altera estado)
+    selA   = 2'b10;
+    selB   = 2'b00;
+    alu_op = 4'b0000;
 
     case (opcode)
       // MOV A,B => A = B (A = 0 + B)
       7'b0000000: begin
-        LA=1; selA=2'b10; selB=2'b00; alu_op=4'b0000; // 0 + B
+        LA     = 1'b1;
+        selA   = 2'b10; // 0
+        selB   = 2'b00; // B
+        alu_op = 4'b0000; // ALU hace ADD
       end
+      
       // MOV B,A => B = A (B = 0 + A)
       7'b0000001: begin
-        LB=1; selA=2'b10; selB=2'b01; alu_op=4'b0000; // 0 + A
+        LB     = 1'b1;
+        selA   = 2'b10; // 0
+        selB   = 2'b01; // A
+        alu_op = 4'b0000; // ALU hace ADD
       end
+
       // MOV A,K => A = K (A = 0 + K)
       7'b0000010: begin
-        LA=1; selA=2'b10; selB=2'b10; alu_op=4'b0000; // 0 + K
+        LA     = 1'b1;
+        selA   = 2'b10; // 0
+        selB   = 2'b10; // K
+        alu_op = 4'b0000; // ALU hace ADD
       end
+
       // MOV B,K => B = K (B = 0 + K)
       7'b0000011: begin
-        LB=1; selA=2'b10; selB=2'b10; alu_op=4'b0000; // 0 + K
+        LB     = 1'b1;
+        selA   = 2'b10; // 0
+        selB   = 2'b10; // K
+        alu_op = 4'b0000; // ALU hace ADD
       end
+
       // ADD A,B => A = A + B
       7'b0000100: begin
-        LA=1; selA=2'b00; selB=2'b00; alu_op=4'b0000;
+        LA     = 1'b1;
+        selA   = 2'b00; // A
+        selB   = 2'b00; // B
+        alu_op = 4'b0000; // ALU hace ADD
       end
-      // ADD B,A => B = B + A
-      7'b0000101: begin
-        LB=1; selA=2'b01; selB=2'b01; alu_op=4'b0000;
-      end
+
       // ADD A,K => A = A + K
       7'b0000110: begin
-        LA=1; selA=2'b00; selB=2'b10; alu_op=4'b0000;
+        LA     = 1'b1;
+        selA   = 2'b00; // A
+        selB   = 2'b10; // K
+        alu_op = 4'b0000; // ALU hace ADD
       end
+
       // ADD B,K => B = B + K
       7'b0000111: begin
-        LB=1; selA=2'b01; selB=2'b10; alu_op=4'b0000;
+        LB     = 1'b1;
+        selA   = 2'b01; // B
+        selB   = 2'b10; // K
+        alu_op = 4'b0000; // ALU hace ADD
       end
+
       // SUB A,B => A = A - B
       7'b0001000: begin
-        LA=1; selA=2'b00; selB=2'b00; alu_op=4'b0001;
+        LA     = 1'b1;
+        selA   = 2'b00; // A
+        selB   = 2'b00; // B
+        alu_op = 4'b0001; // ALU hace SUB
       end
+
       // SUB B,A => B = B - A
       7'b0001001: begin
-        LB=1; selA=2'b01; selB=2'b01; alu_op=4'b0001;
+        LB     = 1'b1;
+        selA   = 2'b01; // B
+        selB   = 2'b01; // A
+        alu_op = 4'b0001; // ALU hace SUB
       end
+
       // SUB A,K => A = A - K
       7'b0001010: begin
-        LA=1; selA=2'b00; selB=2'b10; alu_op=4'b0001;
+        LA     = 1'b1;
+        selA   = 2'b00; // A
+        selB   = 2'b10; // K
+        alu_op = 4'b0001; // ALU hace SUB
       end
+
       // SUB B,K => B = B - K
       7'b0001011: begin
-        LB=1; selA=2'b01; selB=2'b10; alu_op=4'b0001;
+        LB     = 1'b1;
+        selA   = 2'b01; // B
+        selB   = 2'b10; // K
+        alu_op = 4'b0001; // ALU hace SUB
       end
-      // AND A,B => A = A & B
+
+      // AND A,B => A = A and B
       7'b0001100: begin
-        LA=1; selA=2'b00; selB=2'b00; alu_op=4'b0010;
+        LA     = 1'b1;
+        selA   = 2'b00; // A
+        selB   = 2'b00; // B
+        alu_op = 4'b0010; // ALU hace AND
       end
-      // AND B,A => B = B & A
+
+      // AND B,A => B = B and A
       7'b0001101: begin
-        LB=1; selA=2'b01; selB=2'b01; alu_op=4'b0010;
+        LB     = 1'b1;
+        selA   = 2'b01; // B
+        selB   = 2'b01; // A
+        alu_op = 4'b0010; // ALU hace AND
       end
-      // AND A,K => A = A & K
+
+      // AND A,K => A = A and K
       7'b0001110: begin
-        LA=1; selA=2'b00; selB=2'b10; alu_op=4'b0010;
+        LA     = 1'b1;
+        selA   = 2'b00; // A
+        selB   = 2'b10; // K
+        alu_op = 4'b0010; // ALU hace AND
       end
-      // AND B,K => B = B & K
+
+      // AND B,K => B = B and K
       7'b0001111: begin
-        LB=1; selA=2'b01; selB=2'b10; alu_op=4'b0010;
+        LB     = 1'b1;
+        selA   = 2'b01; // B
+        selB   = 2'b10; // K
+        alu_op = 4'b0010; // ALU hace AND
       end
-      // OR A,B => A = A | B
+
+      // OR A,B => A = A or B
       7'b0010000: begin
-        LA=1; selA=2'b00; selB=2'b00; alu_op=4'b0011;
+        LA     = 1'b1;
+        selA   = 2'b00; // A
+        selB   = 2'b00; // B
+        alu_op = 4'b0011; // ALU hace OR
       end
-      // OR B,A => B = B | A
+
+      // OR B,A => B = B or A
       7'b0010001: begin
-        LB=1; selA=2'b01; selB=2'b01; alu_op=4'b0011;
+        LB     = 1'b1;
+        selA   = 2'b01; // B
+        selB   = 2'b01; // A
+        alu_op = 4'b0011; // ALU hace OR
       end
-      // OR A,K => A = A | K
+
+      // OR A,K => A = A or K
       7'b0010010: begin
-        LA=1; selA=2'b00; selB=2'b10; alu_op=4'b0011;
+        LA     = 1'b1;
+        selA   = 2'b00; // A
+        selB   = 2'b10; // K
+        alu_op = 4'b0011; // ALU hace OR
       end
-      // OR B,K => B = B | K
+
+      // OR B,K => B = B or K
       7'b0010011: begin
-        LB=1; selA=2'b01; selB=2'b10; alu_op=4'b0011;
+        LB     = 1'b1;
+        selA   = 2'b01; // B
+        selB   = 2'b10; // K
+        alu_op = 4'b0011; // ALU hace OR
       end
-      // NOT A,A => A = ~A (usa entrada a)
+
+      // NOT A,A => A = ~A
       7'b0010100: begin
-        LA=1; selA=2'b00; alu_op=4'b0101; // selB irrelevante
+        LA     = 1'b1;
+        selA   = 2'b00; // A
+        alu_op = 4'b0101; // ALU hace NOT
       end
-      // NOT A,B => A = ~B (mapeando B a 'a')
+
+      // NOT A,B => A = ~B
       7'b0010101: begin
-        LA=1; selA=2'b01; alu_op=4'b0101; // NOT sobre entrada 'a' (que ahora es B)
+        LA     = 1'b1;
+        selA   = 2'b01; // B
+        alu_op = 4'b0101; // ALU hace NOT
       end
-      // NOT B,A => B = ~A (usa variante NOT sobre b)
+
+      // NOT B,A => B = ~A
       7'b0010110: begin
-        LB=1; selA=2'b00; selB=2'b01; alu_op=4'b0110; // selB=01 => alu_in_b = A, ALU hace ~b = ~A
+        LB     = 1'b1;
+        selA   = 2'b00; // A
+        selB   = 2'b01; // A mapeado a b, o sea ~A
+        alu_op = 4'b0110; // ALU hace NOT B
       end
-      // NOT B,B => B = ~B (usa variante NOT sobre b)
+      
+      // NOT B,B => B = ~B
       7'b0010111: begin
-        LB=1; selA=2'b01; alu_op=4'b0110;
+        LB     = 1'b1;
+        selA   = 2'b01; // B
+        alu_op = 4'b0110; // ALU hace NOT B
       end
-      // XOR A,B => A = A ^ B
+
+      // XOR A,B => A = A xor B
       7'b0011000: begin
-        LA=1; selA=2'b00; selB=2'b00; alu_op=4'b0100;
+        LA     = 1'b1;
+        selA   = 2'b00; // A
+        selB   = 2'b00; // B
+        alu_op = 4'b0100; // ALU hace XOR
       end
-      // XOR B,A => B = B ^ A
+
+      // XOR B,A => B = B xor A
       7'b0011001: begin
-        LB=1; selA=2'b01; selB=2'b01; alu_op=4'b0100;
+        LB     = 1'b1;
+        selA   = 2'b01; // B
+        selB   = 2'b01; // A
+        alu_op = 4'b0100; // ALU hace XOR
       end
-      // XOR A,K => A = A ^ K
+
+      // XOR A,K => A = A xor K
       7'b0011010: begin
-        LA=1; selA=2'b00; selB=2'b10; alu_op=4'b0100;
+        LA     = 1'b1;
+        selA   = 2'b00; // A
+        selB   = 2'b10; // K
+        alu_op = 4'b0100; // ALU hace XOR
       end
-      // XOR B,K => B = B ^ K
+
+      // XOR B,K => B = B xor K
       7'b0011011: begin
-        LB=1; selA=2'b01; selB=2'b10; alu_op=4'b0100;
+        LB     = 1'b1;
+        selA   = 2'b01; // B
+        selB   = 2'b10; // K
+        alu_op = 4'b0100; // ALU hace XOR
       end
-      // SHL A,A => A = A << 1
+
+      // SHL A,A => A = shift left A
       7'b0011100: begin
-        LA=1; selA=2'b00; alu_op=4'b0111;
+        LA     = 1'b1;
+        selA   = 2'b00; // A
+        alu_op = 4'b0111; // ALU hace SHL
       end
-      // SHL A,B => A = B << 1
+
+      // SHL A,B => A = shift left B
       7'b0011101: begin
-        LA=1; selA=2'b01; alu_op=4'b0111;
+        LA     = 1'b1;
+        selA   = 2'b01; // B
+        alu_op = 4'b0111; // ALU hace SHL
       end
-      // SHL B,A => B = A << 1
+
+      // SHL B,A => B = shift left A
       7'b0011110: begin
-        LB=1; selA=2'b00; alu_op=4'b0111;
+        LB     = 1'b1;
+        selA   = 2'b00; // A
+        alu_op = 4'b0111; // ALU hace SHL
       end
-      // SHL B,B => B = B << 1
+
+      // SHL B,B => B = shift left B
       7'b0011111: begin
-        LB=1; selA=2'b01; alu_op=4'b0111;
+        LB     = 1'b1;
+        selA   = 2'b01; // B
+        alu_op = 4'b0111; // ALU hace SHL
       end
-      // SHR A,A => A = A >> 1
+
+      // SHR A,A => A = shift right A
       7'b0100000: begin
-        LA=1; selA=2'b00; alu_op=4'b1000;
+        LA     = 1'b1;
+        selA   = 2'b00; // A
+        alu_op = 4'b1000; // ALU hace SHR
       end
-      // SHR A,B => A = B >> 1
+
+      // SHR A,B => A = shift right B
       7'b0100001: begin
-        LA=1; selA=2'b01; alu_op=4'b1000;
+        LA     = 1'b1;
+        selA   = 2'b01; // B
+        alu_op = 4'b1000; // ALU hace SHR
       end
-      // SHR B,A => B = A >> 1
+
+      // SHR B,A => B = shift right A
       7'b0100010: begin
-        LB=1; selA=2'b00; alu_op=4'b1000;
+        LB     = 1'b1;
+        selA   = 2'b00; // A
+        alu_op = 4'b1000; // ALU hace SHR
       end
-      // SHR B,B => B = B >> 1
+
+      // SHR B,B => B = shift right B
       7'b0100011: begin
-        LB=1; selA=2'b01; alu_op=4'b1000;
+        LB     = 1'b1;
+        selA   = 2'b01; // B
+        alu_op = 4'b1000; // ALU hace SHR
       end
-      // INC B => B = B + 1 (1 + B)
+
+      // INC B => B = B + 1
       7'b0100100: begin
-        LB=1; selA=2'b11; selB=2'b00; alu_op=4'b0000; // (1) + B
+        LB     = 1'b1;
+        selA   = 2'b11; // 1
+        selB   = 2'b00; // B
+        alu_op = 4'b0000; // ALU hace SUMA
       end
 
       default: begin
-        // NOP
+
       end
     endcase
   end
 
-  // MUX A y B (implementados con lógica)
-  wire [7:0] alu_in_a = (selA == 2'b00) ? regA_out_bus :
-                        (selA == 2'b01) ? regB_out_bus :
-                        (selA == 2'b10) ? 8'h00        : 8'h01;
-  wire [7:0] alu_in_b = (selB == 2'b00) ? regB_out_bus :
-                        (selB == 2'b01) ? regA_out_bus :
-                        (selB == 2'b10) ? K            : 8'h00; // 2'b11 => 0
+  // Selecciona la primera entrada de la ALU
+  wire [7:0] alu_in_a = (selA == 2'b00) ? regA_out_bus : // Si selA es 00, selecciona regA
+                        (selA == 2'b01) ? regB_out_bus : // Si selA es 01, selecciona regB
+                        (selA == 2'b10) ? 8'h00 : 8'h01; // Si selA es 10, selecciona '0', si es 11, selecciona '1'
 
-  // Instancias
+  // Selecciona la segunda entrada de la ALU
+  wire [7:0] alu_in_b = (selB == 2'b00) ? regB_out_bus : // Si selB es 00, selecciona regB
+                        (selB == 2'b01) ? regA_out_bus : // Si selB es 01, selecciona regA
+                        (selB == 2'b10) ? K : 8'h00; // Si selB es 10, selecciona K, si es 11, selecciona '0'
+
   pc PC (
       .clk(clk),
       .pc (pc_out_bus)
@@ -218,11 +347,16 @@ module computer (
       .load(LB),
       .out (regB_out_bus)
   );
+  mux2 muxB (
+      .e0 (regB_out_bus),
+      .e1 (im_out_bus[3:0]),
+      .c  (im_out_bus[8]),
+      .out(muxB_out_bus)
+  );
   alu ALU (
-      .a  (alu_in_a),
-      .b  (alu_in_b),
-      .s  (alu_op),
-      .out(alu_out_bus),
-      .Z(alu_Z), .N(alu_N), .C(alu_C), .V(alu_V)
+      .a  (regA_out_bus),
+      .b  (muxB_out_bus),
+      .s  (im_out_bus[5:4]),
+      .out(alu_out_bus)
   );
 endmodule
