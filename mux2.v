1620a435822cd49291393764f817293ca3dbef83
Juan Cabello Vargas <jicabello@miuandes.cl>
2025-09-30 15:04:46 -0300

    Division de mux

--- mux2.v
+++ mux2.v
@@ -1,14 +1,15 @@
-module mux2 (
-    input  [7:0] e0, e1, e2, e3,
-    input  [1:0] sel,
-    output reg [7:0] out
+module muxA (
+  input  [7:0] A, B,
+  input  [7:0] K_unused,      // no se usa en este mux, solo para firma homogénea
+  input  [1:0] sel,
+  output reg [7:0] out
 );
   always @(*) begin
     case (sel)
-      2'b00: out = e0;
-      2'b01: out = e1;
-      2'b10: out = e2;
-      2'b11: out = e3;
+      2'b00: out = A;        // A
+      2'b01: out = B;        // B
+      2'b10: out = 8'h00;    // 0
+      2'b11: out = 8'h01;    // 1
     endcase
   end
 endmodule
